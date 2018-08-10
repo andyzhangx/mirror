e723dc9940b6324b4e5df509f4bce573827e7650
Andy Zhang <xiazhang@microsoft.com>
2018-08-10 16:02:11 +0800

    Update countRegistryRepos.sh

--- zabbix/container/countRegistryRepos.sh
+++ zabbix/container/countRegistryRepos.sh
@@ -2,12 +2,12 @@
 # Below script is used to calc the repository number of current registry
 
 # The following is a sample output:
-# curl -s https://gcr.akscn.io/v2/_catalog
+# curl -s https://127.0.0.1:6001/v2/_catalog
 # {"repositories":["google_containers/defaultbackend","google_containers/hyperkube-amd64","google_containers/nginx-ingress-controller","kubernetes-helm/tiller"]}
 
-site="https://gcr.akscn.io/v2/_catalog"
+site="https://127.0.0.1:6001/v2/_catalog"
 if [ $# -eq 1 ]; then
 	site=$1
 fi
 
-/usr/bin/curl -s $site | grep repositories | awk -F ',' '{print NF}'
+/usr/bin/curl --insecure -s $site | grep repositories | awk -F ',' '{print NF}'
